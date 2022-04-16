69ebd0bb49c12ac1e7853acf5c4caffba97e378b
Oleg Andriienko <whisky@macbook-air-oleg.home>
2022-04-17 00:18:04 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,104 @@
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//     Вивести кожну змінну за допомогою: console.log , alert, document.write
+
+// let a = 'hello';
+// let b = 'owu';
+// let c = 'com';
+// let d = 'ua';
+// let e = '1';
+// let f = '10';
+// let g = '-999';
+// let h = '123';
+// let i = '3.14';
+// let j = 2.7;
+// let k = 16;
+// let l = true;
+// let m = false;
+//
+// console.log(a,b,c,d,e,f,g,h,i,j,k,l,m);
+// document.write(`<div>${a} ${b} ${c}</div>`);
+// alert(`${a} ${b} ${c} ${i} ${m}`);
+
+// - Створити об'єкт book з наступними полями :
+// назва, (тип string)
+// кількість сторінок (числовий тип),
+// жанр (string)
+
+// let book = {
+//     name : 'Геометрія 11 клас',
+//     pages : 300,
+//     genre : 'Хоррор'
+// }
+
+// - Створити об'єкт book з наступними полями :
+// назва, (тип string)
+// кількість сторінок (числовий тип),
+// жанр (string)
+// автори (тип - масив, кожен елемент масиву - це стрінга)
+
+// let book = {
+//     name: 'Геометрія 11 клас',
+//     pages: 300,
+//     genre: 'Хоррор',
+//     authors: ['1','2','3']
+// }
+
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ.
+//     З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+
+// let firstName = 'Oleg';
+// let middleName = 'Andriiovich';
+// let lastName = 'Andriienko';
+//
+// let person = `${lastName} ${firstName} ${middleName}`;
+// console.log(person);
+
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль
+
+// let firstName = prompt();
+// let middleName = prompt();
+// let lastName = prompt();
+//
+// console.log(firstName,middleName,lastName);
+
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//     let a = 100; let b = '100'; let c = true;
+
+// let a = 100;
+// let b = '100';
+// let c = true;
+//
+// console.log(typeof(a))
+// console.log(typeof(b))
+// console.log(typeof(c))
+
+// - Створити масив об'єктів з назвою books з наступними полями  - назва, кількість сторінок, жанр, автори.
+// Вивести кожну книгу як окремий об'єкт
+
+// let books = [
+//     {
+//         name: 'Алгебра',
+//         pages: 400,
+//         genre: 'Horror',
+//         authors: ['1','2','3']
+//     },
+//     {
+//         name: 'Геометрія',
+//         pages: 400,
+//         genre: 'Horror',
+//         authors: ['1','2']
+//     },
+//     {
+//         name: 'Фізика',
+//         pages: 400,
+//         genre: 'Horror',
+//         authors: ['1','2','3']
+//     }
+// ]
+//
+// console.log(books[0]);
+// console.log(books[1]);
+// console.log(books[2]);
+
+
+
